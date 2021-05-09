f906086faf41360c1069938939d687cb37cf1696
Rodrigo Hanunch <rihanuch@uc.cl>
2021-05-08 23:31:59 -0400

    feat: added units to active admin

--- app/admin/properties.rb
+++ app/admin/properties.rb
@@ -1,12 +1,25 @@
 ActiveAdmin.register Property do
-  permit_params :name, :commune, :description, :active, :price, :size, :address, :user_id
+  permit_params(
+    :name,
+    :commune,
+    :description,
+    :active,
+    :price,
+    :size,
+    :address,
+    :user_id,
+    :price_unit,
+    :size_unit
+  )
 
   filter :name
   filter :commune
   filter :active
   filter :user
   filter :price
+  filter :price_unit
   filter :size
+  filter :size_unit
   filter :created_at
   filter :updated_at
 
@@ -18,7 +31,9 @@ ActiveAdmin.register Property do
     column :user
     column :description
     column :price
+    column :price_unit
     column :size
+    column :size_unit
     column :address
     column :active
     actions
