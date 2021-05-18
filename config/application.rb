e8e776139657c28498805eb236a769b875b5c6d0
Jeronimo <jcsalazar@uc.cl>
2021-05-18 10:57:11 -0400

    fix(cors): add middleware to allow all origins and actions

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,11 @@ module Phpeople
     config.active_job.queue_adapter = :sidekiq
     config.assets.paths << Rails.root.join('node_modules')
     config.load_defaults 6.0
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
   end
 end
