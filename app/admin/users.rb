0ed91c6b3913322cd7bdf3fca7db7e54dfa669c9
Rodrigo Hanunch <rihanuch@uc.cl>
2021-05-30 19:31:15 -0400

    feat: roles modification in backoffice

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -13,6 +13,27 @@ ActiveAdmin.register User do
     column :email
     column :name
     column :last_name
+    column :roles
     actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :name
+      f.input :last_name
+      f.input :roles
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :email
+      row :name
+      row :last_name
+      row :roles
+    end
   end
 end
