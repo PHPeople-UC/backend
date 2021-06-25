1903895fad70f4d8316b024479905b27b28ca751
arbiocanpion <lbaguilera@uc.cl>
2021-06-25 02:44:12 -0400

    Added attachment in user profile avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_one_attached :avatar
+  
   validates :name, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z]+\z/ }
   validates :last_name, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z]+\z/ }
 
